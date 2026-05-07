19d4c160280b9a8daf6500101362e2475853e274
313shazid313 <hossainshazid567@gmail.com>
2026-05-07 08:39:31 +0600

    first one

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { RegisterUserDto } from './dtos/userRegister.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post()
+  async registerUser(@Body() user: RegisterUserDto) {
+    const data = await this.userService.registerUser(user);
+    return data;
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from './users.entity';
+import { Repository } from 'typeorm';
+import { RegisterUserDto } from './dtos/userRegister.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(Users)
+    private userRepository: Repository<Users>,
+  ) {}
+
+  public async registerUser(userDto: RegisterUserDto) {
+    const user = await this.userRepository.findOne({
+      where: { email: userDto.email },
+    });
+
+    if (user) {
+      return 'the user with this email already exists';
+    }
+
+    const hashedPassword = await bcrypt.hash(userDto.password, 10);
+
+    let newUser = this.userRepository.create({
+      ...userDto,
+      password: hashedPassword,
+    });
+
+    newUser = await this.userRepository.save(newUser);
+
+    return newUser;
+  }
+}
